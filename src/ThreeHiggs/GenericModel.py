cb5a225196e262d1490c13424f8ed64bb99b3878
Jasmine <jasmine@stp.dias.ie>
2024-06-19 15:29:45 +0100

    Finished dict union and just in time definations

--- src/ThreeHiggs/GenericModel.py
+++ src/ThreeHiggs/GenericModel.py
@@ -39,12 +39,13 @@ class GenericModel():
                 "g2": 2.*MW/ v,                      # SU(2)
                 "g3": m.sqrt(0.1183 * 4.0 * m.pi)}   # SU(3)
         
+        ## --- BSM scalars ---
         if inputParams["bpreCalculated"]:  
             res |= inputParams["couplings"]
             
         else:
             mS1 = inputParams["mS1"]
-    
+            
             if inputParams["bMassSplittingInput"]:
                 mS2, mSpm1, mSpm2 = self.massSplittingsToMasses(mS1, inputParams["delta12"], inputParams["delta1c"], inputParams["deltac"])
     
@@ -53,53 +54,38 @@ class GenericModel():
                 mSpm1 = inputParams["mSpm1"]
                 mSpm2 = inputParams["mSpm2"]
     
-            ## This is the complex phase of lambda_2
-            thetaCPV = inputParams["thetaCPV"]
-            ghDM = inputParams["ghDM"]
-    
-            sinTheta = m.sin(thetaCPV)
-            cosTheta = m.cos(thetaCPV)
-    
-            ## First copy action params that were given as direct inputs
-            res["lam1Re"] = inputParams["lam1Re"]
-            res["lam1Im"] = inputParams["lam1Im"]
-            res["lam11"] = inputParams["lam11"]
-            res["lam22"] = inputParams["lam22"]
-            res["lam12"] = inputParams["lam12"]
-            res["lam12p"] = inputParams["lam12p"]
-    
+            res |= {"lam1Re": inputParams["lam1Re"],
+                    "lam1Im": inputParams["lam1Im"],
+                    "lam11": inputParams["lam11"],
+                    "lam22": inputParams["lam22"],
+                    "lam12": inputParams["lam12"],
+                    "lam12p": inputParams["lam12p"]}
+
             ## Scalar potential parameters. Some of the RHS of these depend on LHS of others, so need to do in smart order
             MHsq = 125.00**2 ## Higgs mass squared
-            res["mu3sq"] = MHsq / 2. 
-            res["lam33"] = MHsq / (2.*v**2) 
+            res |= {"mu3sq": MHsq / 2.,
+                    "lam33": MHsq / (2.*v**2) }
     
-            ### mu12^2 is real in our starting basis but not generally so after loop corrections
             mu12sq = 0.5 * (mSpm2**2 - mSpm1**2)
-            res["mu12sqRe"] = mu12sq
-            res["mu12sqIm"] = 0.0
-    
-            # Abs value of lambda2 (which is complex):
+            res |= {"mu12sqRe": mu12sq,
+                    "mu12sqIm": 0.} ### mu12^2 is real at tree level (basis choice) but generally complex at loop level
+            
+            sinTheta, cosTheta = m.sin(inputParams["thetaCPV"]), m.cos(inputParams["thetaCPV"])
             lam2Abs = 1./v**2 * ( mu12sq*cosTheta + 0.25 * m.sqrt( (2.*mu12sq*cosTheta)**2 + (mS2**2 - mS1**2)**2 - (mSpm2**2 - mSpm1**2)**2 ) )
-    
-            res["lam2Re"] = lam2Abs * cosTheta
-            res["lam2Im"] = lam2Abs * sinTheta
+            res |= {"lam2Re": lam2Abs * cosTheta,
+                    "lam2Im": lam2Abs * sinTheta}
+            
     
             #### Compute some helper parameters
-    
             LambdaMinus = m.sqrt( mu12sq**2 + v**4*lam2Abs**2 - 2.*v**2*mu12sq*lam2Abs*cosTheta) ## Eq 13
             alpha = (-mu12sq + v**2*lam2Abs*cosTheta - LambdaMinus) / ( (v**2*lam2Abs*sinTheta) + 1e-100) ## Eq 12 - Adding 1e-100 to avoid 1/0
     
-            mu2sq = v**2/2. * ghDM - v**2 / (alpha**2 + 1.) * (2.*alpha* sinTheta + (alpha**2 - 1.)*cosTheta) * lam2Abs - 0.5*(mS2**2 + mS1**2)
-    
-            res["mu2sq"] = mu2sq
-    
-            res["lam23"] = 1./v**2 * (2.*mu2sq + mSpm2**2 + mSpm1**2)
-            res["lam23p"] = 1./v**2 * (mS2**2 + mS1**2 - mSpm2**2 - mSpm1**2)
+            mu2sq = v**2/2. * inputParams["ghDM"] - v**2 / (alpha**2 + 1.) * (2.*alpha* sinTheta + (alpha**2 - 1.)*cosTheta) * lam2Abs - 0.5*(mS2**2 + mS1**2)
+            res |= {"mu2sq": mu2sq,
+                    "lam23": 1./v**2 * (2.*mu2sq + mSpm2**2 + mSpm1**2),
+                    "lam23p": 1./v**2 * (mS2**2 + mS1**2 - mSpm2**2 - mSpm1**2)}
         
-    
-            ## Set remaining params
-            if inputParams["darkHierarchy"]:
-                ## eq. (4)
+            if inputParams["darkHierarchy"]: ## eq. (4) but generalised to Hieracy (inputParams["darkHierarchy"] = 1 gives democracy) 
                 res["mu1sq"] = inputParams["darkHierarchy"]*res["mu2sq"]
                 res["lam3Re"] = inputParams["darkHierarchy"]*res["lam2Re"]
                 res["lam3Im"] = inputParams["darkHierarchy"]*res["lam2Im"]
@@ -110,8 +96,7 @@ class GenericModel():
                 print("Input params only implemented in dark hieracy limit!")    
                 raise NotImplementedError 
     
-    
-            res["RGScale"] = inputParams["RGScale"]
+            res["RGScale"] = inputParams["RGScale"] ## This needs to be last due to bug in beta function!!!
 
         return res
     
