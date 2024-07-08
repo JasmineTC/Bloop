73a0a3d61d8ef9dae1973ad0f97cf345a76f7e3d
Jasmine <jasmine@stp.dias.ie>
2024-07-08 15:06:56 +0100

    Added v1,v2,v3 bound args

--- Running/test.sh
+++ Running/test.sh
@@ -4,6 +4,6 @@
 python3 ../src/ThreeHiggs/UnitTests.py
 
 # Super coarse test providing full coverage
-python3 runBenchmark.py -l 2 -n 1 --save --resultsDirectory TestResults --TRangeStart 50 --TRangeEnd 100 --TRangeStepSize 10 --DiagAlgo scipy --minimizationAlgo directGlobal
+python3 runBenchmark.py -l 2 -n 1 --save --resultsDirectory TestResults --TRangeStart 50 --TRangeEnd 100 --TRangeStepSize 10 --DiagAlgo scipy --minimizationAlgo directGlobal --v1Bounds 1e-6 1e-6 --v2Bounds 1e-6 1e-6 --v3Bounds 1e-6 100
 diff TestResults/*.txt ReferenceTestResults/*.txt
 
