1974903fb19bb18a1daad96f841523d3d24cd24e
Lucy Lewitt <ll444@susx.ac.uk>
2024-08-16 12:48:05 +0100

    Add complex mass case to test.sh.

--- Running/test.sh
+++ Running/test.sh
@@ -25,3 +25,25 @@ python3 runBenchmark.py -l 2 \
 
 diff TestResults/BM_1.json ReferenceTestResults/BM_1.json
 
+# Super coarse test providing full coverage, permit complex mass
+python3 runBenchmark.py -l 2 \
+                        --firstBenchmark 1 \
+                        --lastBenchmark 1 \
+                        --save \
+                        --resultsDirectory TestComplexMassResults \
+                        --TRangeStart 50 \
+                        --TRangeEnd 100 \
+                        --TRangeStepSize 10 \
+                        --DiagAlgo scipy \
+                        --minimizationAlgo directGlobal \
+                        --v1Bounds 1e-6 1e-6 \
+                        --v2Bounds 1e-6 1e-6 \
+                        --v3Bounds 1e-6 100 \
+                        --absGlobalTolerance 0.1 \
+                        --relGlobalTolerance 0.1 \
+                        --absLocalTolerance 0.00001 \
+                        --relLocalTolerance 0.00001
+
+diff TestComplexMassResults/BM_1.json ReferenceTestComplexMassResults/BM_1.json
+
+
