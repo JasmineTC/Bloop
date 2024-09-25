a7ab356a9febbb3289f6801464ae932477d0e27a
Jasmine Thomson-Cooke <jasmine@spin.stp.dias.ie>
2024-09-25 13:45:39 +0100

    The correct code for this job

--- Running/FindMissingFiles.py
+++ Running/FindMissingFiles.py
@@ -1,28 +1,15 @@
-import os
+from os.path import join
 from glob import glob ## I think glob lets you do the * thingy
 from json import load
+import numpy as np
 
-count = 0
-for filename in glob(os.path.join('Running/Results/ScanResult', '*.json')):
-    count += 1
-    if count >10:
-        break
-    strongPT = False
-    with open(filename) as json_file:
-        resultDic = load(json_file)
-        if resultDic['jumpsv1']:
-            for jump in resultDic['jumpsv1']:
-                if abs(jump[0]) > 0.7:
-                    strongPT = True
-                    
-        if resultDic['jumpsv2']:
-            for jump in resultDic['jumpsv2']:
-                if abs(jump[0]) > 0.7:
-                    strongPT = True
-        
-        if resultDic['jumpsv3']:
-            for jump in resultDic['jumpsv3']:
-                if abs(jump[0]) > 0.7:
-                    strongPT = True
-    if strongPT:
-        print(filename)
+bmNumList = []
+for filename in glob(join('Results', '*.json')):
+   bmNumList.append(int(filename.split("_")[1]))
+
+bmNumArraySort = np.sort(bmNumList)
+bmNumArrayDiff = np.diff( bmNumArraySort, append = 2 )
+bMissingTag = bmNumArrayDiff >1
+
+print(bmNumArraySort[bMissingTag])
+
