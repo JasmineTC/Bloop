ada43684628a251d020494033c058604b7db2d02
Jasmine Thomson-Cooke <jasmine@spin.stp.dias.ie>
2024-10-10 13:21:41 +0100

    Added SFOPT to find strong phase transitions and scan subset to scan over the sub set of strong transitions. Also deleted PlotResult as it was moved to src and ReadResultsFiles as SFOPT replaces it.

---
+++ Running/SFOPTBM.py
@@ -0,0 +1,17 @@
+from os.path import join
+from glob import glob ## I think glob lets you do the * thingy
+from json import load, dump
+
+strongBmList = []
+
+for fileName in glob(join('Results', '*.json')):
+    with open(fileName, "r") as f:
+        resultDic = load(f)
+        if resultDic["strong"]:
+            strongBmList.append(resultDic["bmNumber"])
+
+print(f"The number of benchmark files tagged as strong is: {len(strongBmList)}")
+
+with open("StrongBM2", "w") as fp:
+    dump(strongBmList, fp)
+
