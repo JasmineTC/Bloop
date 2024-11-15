4c75157db722632e8575b806b358082372cfbed5
Jasmine <jasmine@stp.dias.ie>
2024-11-15 15:59:22 +0000

    Removed redunant benchmarks and made dicts consistent with scan dict

--- Running/Benchmarks/3HDMHandPickedBM.py
+++ Running/Benchmarks/3HDMHandPickedBM.py
@@ -11,7 +11,7 @@ bm0 = {
     ## "Physical" masses of the scalar sector at tree level
     "mS1" : 300,
     "bMassSplittingInput": True, ## Use mass splittings as Venus does or directly put in masses
-    "preCalculated": False, ## Are all non SM couplings calculated before hand or does generic model need to do it?
+    "bPreCalculated": False, ## Are all non SM couplings calculated before hand or does generic model need to do it?
     "delta12" : 0, # mS2 - mS1
     "delta1c" : 0, # mSpm1 - mS1 => set 0 to make lam23p = 0
     "deltac" : 0, # mSpm2 - mSpm1, splitting between charged scalars => if degenerate, mu12^2 = 0 
@@ -56,7 +56,7 @@ bm2 = {
     "bmNumber": 2,
     "mS1" : 57,
     "bMassSplittingInput": True,
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 8.,
     "delta1c" : 50.,
     "deltac" : 1.,
@@ -64,6 +64,7 @@ bm2 = {
     "thetaCPV" : 2.*np.pi/3.,
     "ghDM" : 0.0,
 
+    "darkHierarchy": 1,
     "lam1Re" : 0.1,
     "lam1Im" : 0.0,
     "lam11" : 0.11,
@@ -78,13 +79,14 @@ bm3 = {
     "bmNumber": 3,
     "mS1" : 70,
     "bMassSplittingInput": True,
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 12.,
     "delta1c" : 50.,
     "deltac" : 1.,
 
     "thetaCPV" : 2.*np.pi/3.,
     "ghDM" : 0.0,
+    "darkHierarchy": 1,
 
     "lam1Re" : 0.1,
     "lam1Im" : 0.0,
@@ -100,13 +102,14 @@ bm4 = {
     "bmNumber": 4,
     "mS1" : 48,
     "bMassSplittingInput": True,
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 20.,
     "delta1c" : 50.,
     "deltac" : 1.,
 
     "thetaCPV" : 5.*np.pi/6.,
     "ghDM" : 0.0,
+    "darkHierarchy": 1,
 
     "lam1Re" : 0.1,
     "lam1Im" : 0.0,
@@ -122,13 +125,14 @@ bm5 = {
     "bmNumber": 5,
     "mS1" : 75,
     "bMassSplittingInput": True,
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 55.,
     "delta1c" : 50.,
     "deltac" : 1.,
 
     "thetaCPV" : 2.*np.pi/3.,
     "ghDM" : 0.0,
+    "darkHierarchy": 1,
 
     "lam1Re" : 0.1,
     "lam1Im" : 0.0,
@@ -144,13 +148,14 @@ bm6 = {
     "bmNumber": 6,
     "mS1" : 74,
     "bMassSplittingInput": True, 
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 55.,
     "delta1c" : 50.,
     "deltac" : 15.,
 
     "thetaCPV" : 2.*np.pi/3.,
     "ghDM" : 0.0,
+    "darkHierarchy": 1,
 
     "lam1Re" : 0.1,
     "lam1Im" : 0.0,
@@ -166,13 +171,14 @@ bm7 = {
     "bmNumber": 7,
     "mS1" : 90,
     "bMassSplittingInput": True,
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 5.,
     "delta1c" : 1.,
     "deltac" : 1.,
 
     "thetaCPV" : 2.*np.pi/3.,
     "ghDM" : 0.0,
+    "darkHierarchy": 1,
 
     "lam1Re"    : 0.1,
     "lam1Im" : 0.0,
@@ -188,13 +194,14 @@ bm8 = {
     "bmNumber": 8,
     "mS1" : 90,
     "bMassSplittingInput": True,
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 55.,
     "delta1c" : 1.,
     "deltac" : 1.,
 
     "thetaCPV" : 2.*np.pi/3.,
     "ghDM" : 0.0,
+    "darkHierarchy": 1,
 
     "lam1Re" : 0.1,
     "lam1Im" : 0.0,
@@ -210,13 +217,14 @@ bm9 = {
     "bmNumber": 9,
     "mS1" : 90,
     "bMassSplittingInput": True,
-    "preCalculated": False,
+    "bPreCalculated": False,
     "delta12" : 55.,
     "delta1c" : 1.,
     "deltac" : 22.,
 
     "thetaCPV" : 2.*np.pi/3.,
     "ghDM" : 0.0,
+    "darkHierarchy": 1,
 
     "lam1Re" : 0.1,
     "lam1Im" : 0.0,
@@ -227,227 +235,6 @@ bm9 = {
 }
 bmList.append(bm9)
 
-##Fake benchmark that is quick to run to keep naming convention the same as draft
-bm10 = {
-    "RGScale" : __MZ,
-    "bmNumber": 10,
-    "mS1" : 300,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 0,
-    "delta1c" : 0,
-    "deltac" : 0, 
-
-    "thetaCPV" : 0.0,
-    "ghDM" : 0.0,
-
-    "lam1Re" : 1e-12,
-    "lam1Im" : 1e-12,
-    "lam11" : 1e-12,
-    "lam22" : 1e-12,
-    "lam12" : 1e-12,
-    "lam12p" : 1e-12
-}
-bmList.append(bm10)
-##Same as benchmark as 10 before it but with higgs dark matter coupling turned on
-bm11 = {
-    "RGScale" : __MZ,
-    "bmNumber": 11,
-    "mS1" : 67,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 4.,
-    "delta1c" : 50.,
-    "deltac" : 1.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm11)
-
-bm12 = {
-    "RGScale" : __MZ,
-    "bmNumber": 12,
-    "mS1" : 57,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 8.,
-    "delta1c" : 50.,
-    "deltac" : 1.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm12)
-
-bm13 = {
-    "RGScale" : __MZ,
-    "bmNumber": 13,
-    "mS1" : 70,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 12.,
-    "delta1c" : 50.,
-    "deltac" : 1.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm13)
-
-bm14 = {
-    "RGScale" : __MZ,
-    "bmNumber": 14,
-    "mS1" : 48,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 20.,
-    "delta1c" : 50.,
-    "deltac" : 1.,
-
-    "thetaCPV" : 5.*np.pi/6.,
-    "ghDM" : 0.01,
-    
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm14)
-
-bm15 = {
-    "RGScale" : __MZ,
-    "bmNumber": 15,
-    "mS1" : 75,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 55.,
-    "delta1c" : 50.,
-    "deltac" : 1.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm15)
-
-bm16 = {
-    "RGScale" : __MZ,
-    "bmNumber": 16,
-    "mS1" : 74,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 55.,
-    "delta1c" : 50.,
-    "deltac" : 15.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm16)
-
-bm17 = {
-    "RGScale" : __MZ,
-    "bmNumber": 18,
-    "mS1" : 90,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 5.,
-    "delta1c" : 1.,
-    "deltac" : 1.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm17)
-
-bm18 = {
-    "RGScale" : __MZ,
-    "bmNumber": 18,
-    "mS1" : 90,
-    "bMassSplittingInput": True, 
-    "preCalculated": False,
-    "delta12" : 55.,
-    "delta1c" : 1.,
-    "deltac" : 1.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm18)
-
-bm19 = {
-    "RGScale" : __MZ,
-    "bmNumber": 19,
-    "mS1" : 90,
-    "bMassSplittingInput": True,
-    "preCalculated": False,
-    "delta12" : 55.,
-    "delta1c" : 1.,
-    "deltac" : 22.,
-
-    "thetaCPV" : 2.*np.pi/3.,
-    "ghDM" : 0.02,
-
-    "lam1Re" : 0.1,
-    "lam1Im" : 0.0,
-    "lam11" : 0.11,
-    "lam22" : 0.12,
-    "lam12" : 0.13,
-    "lam12p" : 0.14
-}
-bmList.append(bm19)
-
 import json
-with open('Benchmarks_3HDM.json', 'w') as file:
+with open('Benchmarks/Benchmarks_3HDM.json', 'w') as file:
     json.dump(bmList, file, indent=4)
