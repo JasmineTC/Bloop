b41a71cc3c192432f39909439304091db92c9dca
Jasmine <jasmine@stp.dias.ie>
2024-11-28 16:00:53 +0000

    Updated random scan code to make room for not random scan - NOT INTEGRATED YET

--- Running/Benchmarks/3HDMScanBM.py
+++ Running/Benchmarks/3HDMScanBM.py
@@ -27,7 +27,7 @@ def bNoLightCharged(mSpm1, mSpm2) -> bool:
     return mSpm1 >= 90 and \
            mSpm2 >= 90
 
-def bmListGen():
+def lagranianParamGen(mS1, delta12, delta1c, deltac, ghDM, thetaCPV, darkHieracy, bmNumber):
     vsq = 246.22**2
     ## Some 'dark' sector params we keep fixed to keep the scan managable
     lam1Re = 0.1
@@ -42,11 +42,82 @@ def bmListGen():
     ## Helper param
     lamx = lam12 + min(0, lam12p - 2*m.sqrt(lam1Re**2 + lam1Im**2) )
     
-    bmNumber = 0
-    ThreeHiggsBMDictList = []
-    count = 0
-    while bmNumber <= 1e6:
-        count+=1
+
+    mS2 = delta12 + mS1
+    mSpm1 = delta1c + mS1
+    mSpm2 = deltac + mSpm1
+    mu12sq = (mSpm2**2 - mSpm1**2)/2
+    
+    sinTheta, cosTheta = m.sin(thetaCPV), m.cos(thetaCPV)
+    lam2absInsideSqR = (2.*mu12sq*cosTheta)**2 + (mS2**2 - mS1**2)**2 - (mSpm2**2 - mSpm1**2)**2
+    if lam2absInsideSqR < 0:
+        return False
+    
+    lam2Abs = ( mu12sq*cosTheta + m.sqrt( lam2absInsideSqR )/4 ) /vsq
+    lam2Re = lam2Abs*cosTheta
+    lam2Im = lam2Abs*sinTheta
+    
+    lambdaMinus = m.sqrt( mu12sq**2 + vsq**2*lam2Abs**2 - 2.*vsq*mu12sq*lam2Abs*cosTheta)
+    lambdaPlus = m.sqrt( mu12sq**2 + vsq**2*lam2Abs**2 + 2.*vsq*mu12sq*lam2Abs*cosTheta)
+    alpha = (-mu12sq + vsq*lam2Abs*cosTheta - lambdaMinus) / ( (vsq*lam2Abs*sinTheta) )
+    mu2sq = vsq/2. * ghDM - vsq / (alpha**2 + 1.) * (2.*alpha* sinTheta + (alpha**2 - 1.)*cosTheta) * lam2Abs - (mS2**2 + mS1**2)/2
+    lam23 = (2.*mu2sq + mSpm2**2 + mSpm1**2)/vsq
+    lam23p = (mS2**2 + mS1**2 - mSpm2**2 - mSpm1**2)/vsq
+    
+    mu1sq = darkHieracy*mu2sq
+    lam3Re = darkHieracy*lam2Re
+    lam3Im = darkHieracy*lam2Im
+    lam31 = darkHieracy*lam23
+    lam31p = darkHieracy*lam23p
+    
+    lamy = lam31 + min(0, lam31p - 2*m.sqrt(lam3Re**2 + lam3Im**2) )
+    lamz = lam23 + min(0, lam23p - 2*m.sqrt(lam2Re**2 + lam2Im**2) )
+
+    if bIsBounded(lam11, lam22, lam33, lamx, lamy, lamz) \
+        and bNoLightCharged(mSpm1, mSpm2) \
+        and bPositiveMassStates(mu2sq, mu12sq, lam23, lam23p, lambdaMinus, lambdaPlus, vsq):
+        return{
+            "bmNumber": bmNumber,
+            "bPreCalculated": True,
+            "bMassSplittingInput": False,
+            "RGScale": 91.1876,
+            
+            "bmInput": {"thetaCPV" : thetaCPV,
+                         "ghDM" : ghDM,
+                         "mS1" : mS1,
+                         "delta12" : delta12,
+                         "delta1c" : delta1c,
+                         "deltac" : deltac,
+                         "darkHieracy": darkHieracy},
+            
+            "massTerms": {"mu12sqRe" : mu12sq, 
+                          "mu12sqIm" : 0, 
+                          "mu2sq": mu2sq, 
+                          "mu3sq":mu3sq,
+                          "mu1sq": mu1sq,},
+                                
+            "couplingValues":{"lam1Re" : lam1Re, 
+                              "lam1Im" : lam1Im, 
+                              "lam2Re" : lam2Abs*cosTheta, 
+                              "lam2Im" : lam2Abs*sinTheta, 
+                              "lam11" : lam11, 
+                              "lam22" : lam22, 
+                              "lam12" : lam12, 
+                              "lam12p" : lam12p, 
+                              "lam23": lam23,
+                              "lam23p": lam23p,
+                              "lam3Re": lam3Re,
+                              "lam3Im": lam3Im,
+                              "lam31": lam31,
+                              "lam31p": lam31p,
+                              "lam33": lam33}
+            
+        }
+    return False
+
+def randomBmParam():
+    bmList = []
+    while len(bmList) < 3:
         mS1 = np.random.uniform(63, 100)
         delta12 = np.random.uniform(5, 100)
         delta1c = np.random.uniform(5, 100)
@@ -54,80 +125,13 @@ def bmListGen():
         ghDM = np.random.uniform(0, 1)
         thetaCPV = np.random.uniform(np.pi/2, 3*np.pi/2)
         darkHieracy = 1
-        
-        mS2 = delta12 + mS1
-        mSpm1 = delta1c + mS1
-        mSpm2 = deltac + mSpm1
-        mu12sq = (mSpm2**2 - mSpm1**2)/2
-        
-        sinTheta, cosTheta = m.sin(thetaCPV), m.cos(thetaCPV)
-        lam2absInsideSqR = (2.*mu12sq*cosTheta)**2 + (mS2**2 - mS1**2)**2 - (mSpm2**2 - mSpm1**2)**2
-        if lam2absInsideSqR < 0:
-            continue
-        
-        lam2Abs = ( mu12sq*cosTheta + m.sqrt( lam2absInsideSqR )/4 ) /vsq
-        lam2Re = lam2Abs*cosTheta
-        lam2Im = lam2Abs*sinTheta
-        
-        lambdaMinus = m.sqrt( mu12sq**2 + vsq**2*lam2Abs**2 - 2.*vsq*mu12sq*lam2Abs*cosTheta)
-        lambdaPlus = m.sqrt( mu12sq**2 + vsq**2*lam2Abs**2 + 2.*vsq*mu12sq*lam2Abs*cosTheta)
-        alpha = (-mu12sq + vsq*lam2Abs*cosTheta - lambdaMinus) / ( (vsq*lam2Abs*sinTheta) )
-        mu2sq = vsq/2. * ghDM - vsq / (alpha**2 + 1.) * (2.*alpha* sinTheta + (alpha**2 - 1.)*cosTheta) * lam2Abs - (mS2**2 + mS1**2)/2
-        lam23 = (2.*mu2sq + mSpm2**2 + mSpm1**2)/vsq
-        lam23p = (mS2**2 + mS1**2 - mSpm2**2 - mSpm1**2)/vsq
-        
-        mu1sq = darkHieracy*mu2sq
-        lam3Re = darkHieracy*lam2Re
-        lam3Im = darkHieracy*lam2Im
-        lam31 = darkHieracy*lam23
-        lam31p = darkHieracy*lam23p
-        
-        lamy = lam31 + min(0, lam31p - 2*m.sqrt(lam3Re**2 + lam3Im**2) )
-        lamz = lam23 + min(0, lam23p - 2*m.sqrt(lam2Re**2 + lam2Im**2) )
+        bmDict = lagranianParamGen(mS1, delta12, delta1c, deltac, ghDM, thetaCPV, darkHieracy, len(bmList))
+        if bmDict:
+            bmList.append(bmDict)
+    return bmList
 
-        if bIsBounded(lam11, lam22, lam33, lamx, lamy, lamz) \
-            and bNoLightCharged(mSpm1, mSpm2) \
-            and bPositiveMassStates(mu2sq, mu12sq, lam23, lam23p, lambdaMinus, lambdaPlus, vsq):
-            ThreeHiggsBMDictList.append({
-                "bmNumber": bmNumber,
-                "bPreCalculated": True,
-                "bMassSplittingInput": False,
-                "RGScale": 91.1876,
-                
-                "bmInput": {"thetaCPV" : thetaCPV,
-                             "ghDM" : ghDM,
-                             "mS1" : mS1,
-                             "delta12" : delta12,
-                             "delta1c" : delta1c,
-                             "deltac" : deltac,
-                             "darkHieracy": darkHieracy},
-                
-                "massTerms": {"mu12sqRe" : mu12sq, 
-                              "mu12sqIm" : 0, 
-                              "mu2sq": mu2sq, 
-                              "mu3sq":mu3sq,
-                              "mu1sq": mu1sq,},
-                                    
-                "couplingValues":{"lam1Re" : lam1Re, 
-                                  "lam1Im" : lam1Im, 
-                                  "lam2Re" : lam2Abs*cosTheta, 
-                                  "lam2Im" : lam2Abs*sinTheta, 
-                                  "lam11" : lam11, 
-                                  "lam22" : lam22, 
-                                  "lam12" : lam12, 
-                                  "lam12p" : lam12p, 
-                                  "lam23": lam23,
-                                  "lam23p": lam23p,
-                                  "lam3Re": lam3Re,
-                                  "lam3Im": lam3Im,
-                                  "lam31": lam31,
-                                  "lam31p": lam31p,
-                                  "lam33": lam33}
-                
-            })
-            bmNumber+=1 
-    return ThreeHiggsBMDictList
+print(randomBmParam() )
 
-from json import dump                       
-with open("3HDMScanBMTest.json", "w") as outfile: 
-    dump(bmListGen(), outfile, indent = 4)
+# from json import dump                       
+# with open("3HDMScanBMTest.json", "w") as outfile: 
+#     dump(bmListGen(), outfile, indent = 4)
