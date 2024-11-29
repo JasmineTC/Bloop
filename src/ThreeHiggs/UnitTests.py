3431eff22137aee4e545c65491f45ae4ecdcaf4f
Jasmine <jasmine@stp.dias.ie>
2024-11-29 15:51:41 +0000

    BmGen now called

--- src/ThreeHiggs/UnitTests.py
+++ src/ThreeHiggs/UnitTests.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from ThreeHiggs.ParsedExpression import ParsedExpressionUnitTests
     from ThreeHiggs.EffectivePotential import EffectivePotentialUnitTests
     from ThreeHiggs.TransitionFinder import TransitionFinderUnitTests
-
+    from ThreeHiggs.BmGenerator import BmGeneratorUnitTests
     from unittest import main
     main()
 
