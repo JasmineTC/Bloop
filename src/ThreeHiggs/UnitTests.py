83c52d3be63cd5834c4ca414f6f9be4194682b73
jasmine <jasmine@stp.dias.ie>
2025-01-16 13:12:48 +0000

    Added integration test for getEFTParams

--- src/ThreeHiggs/UnitTests.py
+++ src/ThreeHiggs/UnitTests.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     from ThreeHiggs.EffectivePotential import EffectivePotentialUnitTests
     from ThreeHiggs.TransitionFinder import TransitionFinderUnitTests
     from ThreeHiggs.BmGenerator import BmGeneratorUnitTests
+    from ThreeHiggs.DimensionalReduction import DimensionalReductionUnitTest
     from unittest import main
     main()
 
