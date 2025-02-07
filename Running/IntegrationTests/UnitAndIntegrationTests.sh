b5f57b54fca45923a1e27ec4114da22ac8ea09d8
Jasmine <jasmine@stp.dias.ie>
2025-02-07 11:49:11 +0000

    Added integration test for pool with 1 coreq

--- Running/IntegrationTests/UnitAndIntegrationTests.sh
+++ Running/IntegrationTests/UnitAndIntegrationTests.sh
@@ -6,6 +6,23 @@ python3 ../src/ThreeHiggs/UnitTests.py
 
 echo Intergration tests...
 # Super coarse test providing full coverage
+
+echo Running code at NLO with pool...
+rm -f IntegrationTests/Pool/OutputResult/* 
+rm -f IntegrationTests/Benchmarks/*
+python3 runStages.py --loopOrder 1 \
+                        --firstBenchmark 1 \
+                        --lastBenchmark 1 \
+                        --bSave \
+                        --resultsDirectory IntegrationTests/Pool/OutputResult/  \
+                        --benchmarkFile IntegrationTests/Benchmarks \
+                        --TRangeStart 100 \
+                        --TRangeEnd 200 \
+                        --TRangeStepSize 2 \
+                        --bPool \
+
+diff IntegrationTests/Pool/OutputResult/BM_1.json IntegrationTests/Pool/ReferenceResult/BM_1.json
+
 echo Running code at NLO with pool using 2 cores...
 rm -f IntegrationTests/Pool/OutputResult/* 
 rm -f IntegrationTests/Benchmarks/*
@@ -20,10 +37,13 @@ python3 runStages.py --loopOrder 1 \
                         --TRangeStepSize 2 \
                         --bPool \
                         --cores 2
-diff IntegrationTests/Pool/OutputResult/BM_0.json IntegrationTests/Pool/ReferenceResult/BM_0.json
-diff IntegrationTests/Pool/OutputResult/BM_1.json IntegrationTests/Pool/ReferenceResult/BM_1.json
-diff IntegrationTests/Pool/OutputResult/BM_2.json IntegrationTests/Pool/ReferenceResult/BM_2.json
-diff IntegrationTests/Pool/OutputResult/BM_3.json IntegrationTests/Pool/ReferenceResult/BM_3.json
+
+diff IntegrationTests/Pool2/OutputResult/BM_0.json IntegrationTests/Pool2/ReferenceResult/BM_0.json
+diff IntegrationTests/Pool2/OutputResult/BM_1.json IntegrationTests/Pool2/ReferenceResult/BM_1.json
+diff IntegrationTests/Pool2/OutputResult/BM_2.json IntegrationTests/Pool2/ReferenceResult/BM_2.json
+diff IntegrationTests/Pool2/OutputResult/BM_3.json IntegrationTests/Pool2/ReferenceResult/BM_3.json
+
+exit
 echo Running code at NNLO...
 rm -f IntegrationTests/ComplexMass/OutputResult/* 
 python3 runStages.py --loopOrder 2 \
