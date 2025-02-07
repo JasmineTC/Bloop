65d9946e3962d230c9c54df8bea517332279abd0
Jasmine <jasmine@stp.dias.ie>
2025-02-07 11:39:17 +0000

    Undid path spec changes

--- Running/IntegrationTests/UnitAndIntegrationTests.sh
+++ Running/IntegrationTests/UnitAndIntegrationTests.sh
@@ -2,54 +2,54 @@
 
 # Fine tests providing partial coverage
 echo Unit tests...
-python3 ../../src/ThreeHiggs/UnitTests.py
+python3 ../src/ThreeHiggs/UnitTests.py
 
 echo Intergration tests...
 # Super coarse test providing full coverage
 echo Running code at NLO with pool using 2 cores...
-rm -f Pool/OutputResult/* 
+rm -f IntegrationTests/Pool/OutputResult/* 
 python3 runStages.py --loopOrder 1 \
                         --firstBenchmark 0 \
                         --lastBenchmark 3 \
                         --bSave \
-                        --resultsDirectory Pool/OutputResult/  \
-                        --TRangeStart 100 \
+                        --resultsDirectory IntegrationTests/Pool/OutputResult/  \
+                        --TRangeStart 50 \
                         --TRangeEnd 200 \
                         --TRangeStepSize 2 \
                         --bPool \
                         --cores 2
-diff Pool/OutputResult/BM_0.json Pool/ReferenceResult/BM_0.json
-diff Pool/OutputResult/BM_1.json Pool/ReferenceResult/BM_1.json
-diff Pool/OutputResult/BM_2.json Pool/ReferenceResult/BM_2.json
-diff Pool/OutputResult/BM_3.json Pool/ReferenceResult/BM_3.json
+diff IntegrationTests/Pool/OutputResult/BM_0.json IntegrationTests/Pool/ReferenceResult/BM_0.json
+diff IntegrationTests/Pool/OutputResult/BM_1.json IntegrationTests/Pool/ReferenceResult/BM_1.json
+diff IntegrationTests/Pool/OutputResult/BM_2.json IntegrationTests/Pool/ReferenceResult/BM_2.json
+diff IntegrationTests/Pool/OutputResult/BM_3.json IntegrationTests/Pool/ReferenceResult/BM_3.json
 
-echo Intergration test: Running code at NNLO...
-rm -f ComplexMass/OutputResult/* 
+echo Running code at NNLO...
+rm -f IntegrationTests/ComplexMass/OutputResult/* 
 python3 runStages.py --loopOrder 2 \
                         --firstBenchmark 1 \
                         --lastBenchmark 1 \
                         --bSave \
-                        --resultsDirectory ComplexMass/OutputResult/  \
+                        --resultsDirectory IntegrationTests/ComplexMass/OutputResult/  \
                         --TRangeStart 50 \
                         --TRangeEnd 100 \
                         --TRangeStepSize 10 \
                         --bProcessMin
-diff ComplexMass/OutputResult/BM_1.json ComplexMass/ReferenceResult/BM_1.json
-diff ComplexMass/OutputResult/BM_1_interp.json ComplexMass/ReferenceResult/BM_1_interp.json
+diff IntegrationTests/ComplexMass/OutputResult/BM_1.json IntegrationTests/ComplexMass/ReferenceResult/BM_1.json
+diff IntegrationTests/ComplexMass/OutputResult/BM_1_interp.json IntegrationTests/ComplexMass/ReferenceResult/BM_1_interp.json
 
 
-echo Intergration test: Running code at NNLO with Numba...
-rm -f Numba/OutputResult/* 
+echo Running code at NNLO with Numba...
+rm -f IntegrationTests/Numba/OutputResult/* 
 python3 runStages.py --loopOrder 2 \
                         --firstBenchmark 1 \
                         --lastBenchmark 1 \
                         --bSave \
-                        --resultsDirectory Numba/OutputResult/ \
+                        --resultsDirectory IntegrationTests/Numba/OutputResult/ \
                         --TRangeStart 50 \
                         --TRangeEnd 100 \
                         --TRangeStepSize 10 \
                         --bNumba \
-                        --bProcessMini
-diff Numba/OutputResult/BM_1.json Numba/ReferenceResult/BM_1.json
-diff Numba/OutputResult/BM_1_interp.json Numba/ReferenceResult/BM_1_interp.json
+                        --bProcessMin
+diff IntegrationTests/Numba/OutputResult/BM_1.json IntegrationTests/Numba/ReferenceResult/BM_1.json
+diff IntegrationTests/Numba/OutputResult/BM_1_interp.json IntegrationTests/Numba/ReferenceResult/BM_1_interp.json
 
