7da09b64a278e52f0c54534af420dca2243a13c4
Jasmine <jasmine@stp.dias.ie>
2025-02-14 16:53:45 +0000

    v3 now underscored

--- src/ThreeHiggs/PlotResult.py
+++ src/ThreeHiggs/PlotResult.py
@@ -13,9 +13,9 @@ def plotData(result: dict, index : int, loopOrder : int, filename : str): #->png
     v2List = makeFieldDimensionless(tempList, result["minimumLocation"][1])
     v3List = makeFieldDimensionless(tempList, result["minimumLocation"][2])
 
-    plt.plot(tempList, v1List, 'v', label = "v1", color = 'orange')
-    plt.plot(tempList, v2List, '^', label = "v2", color = 'black')
-    plt.plot(tempList, v3List, '.', label = "v3", color = 'blue')
+    plt.plot(tempList, v1List, 'v', label = "$v_1$", color = 'orange')
+    plt.plot(tempList, v2List, '^', label = "$v_2$", color = 'black')
+    plt.plot(tempList, v3List, '.', label = "$v_3$", color = 'blue')
     plt.legend(loc = 'best')
     #plt.title(f"Field values at global minimum for benchmark {index} at loop order {loopOrder}")
     plt.ylabel(r"$\dfrac{v}{\sqrt{T}}$", rotation = 0, labelpad = 10)
