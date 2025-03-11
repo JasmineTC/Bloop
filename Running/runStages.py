7263ff472d28bb3c59836a1b0c03cb133861e5c4
Jasmine <jasmine@stp.dias.ie>
2025-03-11 11:18:36 +0000

    Remove debugging code

--- Running/runStages.py
+++ Running/runStages.py
@@ -1,11 +1,6 @@
 
 from ThreeHiggs.UserInput import UserInput, Stages
 args = UserInput().parse()
-# print(args)
-# open("test.json", "w").write(json.dumps(vars(args), indent = 4))
-# loadArgs = (json.load(open("test.json", "r")))
-# print(vars(args) | loadArgs)
-# print(UserInput().parse())
 if args.firstStage <= Stages.convertMathematica <= args.lastStage:
     from ThreeHiggs.ConvertMathematica import convertMathematica
     convertMathematica(args)
