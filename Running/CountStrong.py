a39be3a72e15bf5ec553f823eb1a96555ee23029
Jasmine Thomson-Cooke <jasmine@stp.dias.ie>
2025-03-31 15:18:04 +0100

    Now (lazily) prints tc min

--- Running/CountStrong.py
+++ Running/CountStrong.py
@@ -6,7 +6,7 @@ If the benchmark is strong save the file to a subsetresult dir'''
 from os.path import join
 from glob import glob ## I think glob lets you do the * thingy
 from json import load, dump
-directory = "1LoopResults/MaximalCPV01"
+directory = "2LoopResults/Filler01"
 totalCount = 0
 strongCount = 0
 mutliV3Jump = 0
@@ -15,6 +15,7 @@ complexCount = 0
 from collections import defaultdict
 failDict = defaultdict(int)
 
+TcMin = 1e100
 for filePointer in glob(join(directory, '*.json')):
     fileName = filePointer.split('/')[1].lstrip().split(' ')[0]
     with open(filePointer, "r") as f:
@@ -24,14 +25,19 @@ for filePointer in glob(join(directory, '*.json')):
             failDict[resultDic["failureReason"]] +=1
         else:
             if len(resultDic["jumpsv3"])>1:
+                print(resultDic)
                 mutliV3Jump += 1
             if resultDic["strong"] >0.6:
                 strongCount += 1
+                TcMin = resultDic["jumpsv3"][0][1] if resultDic["jumpsv3"][0][1] < TcMin else TcMin
             if resultDic["step"] > 1:
                 multiStepCount += 1
+                if resultDic["strong"]>0.6:
+                    print(resultDic["strong"])
             if resultDic["complexMin"]:
                 complexCount += 1
 print(f"Summary of the results in the directory '{directory}':")
+print(TcMin)
 print(f"The number of benchmarks is: {totalCount}")
 print(f"The number of strong benchmarks is: {strongCount}")
 print(f"The number of mutli step phase transitions is: {multiStepCount}")
