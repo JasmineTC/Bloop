7b4c0112daaef15b2e5eca8c9458e4e8c2127ecd
Jasmine <jasmine@stp.dias.ie>
2025-04-29 14:08:28 +0100

    Removed debugging code

--- src/runStages.py
+++ src/runStages.py
@@ -1,6 +1,6 @@
-
 from ThreeHiggs.UserInput import UserInput, Stages
 args = UserInput().parse()
+
 if args.firstStage <= Stages.convertMathematica <= args.lastStage:
     from ThreeHiggs.PythoniseMathematica import pythoniseMathematica
     pythoniseMathematica(args)
@@ -12,4 +12,3 @@ if args.firstStage <= Stages.generateBenchmark <= args.lastStage:
 if args.firstStage <= Stages.doMinimization <= args.lastStage:
     from ThreeHiggs.DoMinimization import minimization
     minimization(args)
-    
