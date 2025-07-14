e95d1dca0716d792e04dee061974931ac0ff1b33
Jasmine <jasmine@stp.dias.ie>
2025-07-14 11:43:49 +0100

    Renamed bVerbose to verbose

--- src/runStages.py
+++ src/runStages.py
@@ -3,22 +3,22 @@ from ThreeHiggs.UserInput import UserInput, Stages
 args = UserInput().parse()
 
 if args.firstStage <= Stages.convertMathematica <= args.lastStage:
-    if args.bVerbose:
+    if args.verbose:
         print("Convert Mathematica stage started")
 
     from ThreeHiggs.PythoniseMathematica import pythoniseMathematica
     pythoniseMathematica(args)
 
 if args.firstStage <= Stages.generateBenchmark <= args.lastStage:
-    if args.bVerbose:
+    if args.verbose:
         print("Benchmark generation stage started")
         
     from ThreeHiggs.BmGenerator import generateBenchmarks
     generateBenchmarks(args)
 
 if args.firstStage <= Stages.doMinimization <= args.lastStage:
-    if args.bVerbose:
-        print("do minimization stage started")
+    if args.verbose:
+        print("Minimization stage started")
     from ThreeHiggs.DoMinimization import minimization
     minimization(args)
     
