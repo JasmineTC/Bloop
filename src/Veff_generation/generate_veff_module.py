61aac3ff1eb2b676e31781c34a060ccf83a02541
Lucy Lewitt <ll444@susx.ac.uk>
2025-08-03 19:11:21 +0100

    Fix compilation errors.

--- src/Veff_generation/generate_veff_module.py
+++ src/Veff_generation/generate_veff_module.py
@@ -12,25 +12,6 @@ import numpy as np
 from .mathematica_parsing import read_lines, get_terms
 
 
-SETUP_FILE = """
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-
-extensions = [
-    Extension("lo", ["lo.pyx"]),
-    Extension("nlo", ["nlo.pyx"]),
-    Extension("nnlo", ["nnlo.pyx"]),
-]
-
-setup(
-    name="Veff_cython",
-    ext_modules=cythonize(
-        extensions, compiler_directives={"language_level": "3"}
-    ),
-)
-"""
 
 
 
@@ -54,13 +35,13 @@ def generate_veff_module(args):
     name     = 'nlo'
     nlo_file = os.path.join(data_dir, args.nloFile)
     filename = os.path.join(module_dir, 'nlo.pyx')
-    nlo_params = generate_lo_submodule(name, filename, nlo_file)
+    nlo_params = generate_lo_submodule(name, filename, nlo_file) if args.loopOrder > 0 else None
     
     #================================== nnlo =================================#
     name = 'nnlo'
     nnlo_file = os.path.join(data_dir, args.nnloFile)
     filename  = os.path.join(module_dir, 'nnlo.pyx')
-    nnlo_params = generate_lo_submodule(name, filename, nnlo_file)
+    nnlo_params = generate_lo_submodule(name, filename, nnlo_file) if args.loopOrder > 1 else None
     
     #================================== Veff =================================#
     filename = os.path.join(module_dir, 'veff.py')
@@ -72,10 +53,31 @@ def generate_veff_module(args):
     
     #=============================== setup file ==============================#
     with open(os.path.join(module_dir, 'setup.py'), 'w') as file:
-        file.writelines(SETUP_FILE)
+        from textwrap import dedent
+        from jinja2 import Environment
+        file.writelines(Environment().from_string(dedent("""\
+            #!/usr/bin/env python3
+            # -*- coding: utf-8 -*-
+            from setuptools import setup, Extension
+            from Cython.Build import cythonize
+            
+            extensions = [Extension("lo", ["lo.pyx"])]
+            {% if args.loopOrder >= 1 %}
+            extensions.append(Extension("nlo", ["nlo.pyx"]))
+            {% endif %}
+            {% if args.loopOrder >= 2 %}
+            extensions.append(Extension("nnlo", ["nnlo.pyx"]))
+            {% endif %}
+            
+            setup(
+                name="Veff_cython",
+                ext_modules=cythonize(
+                    extensions, compiler_directives={"language_level": "3"}
+                ),
+            )
+            """
+        )).render(args = args))
         
-
-
 def generate_veff_submodule(filename, lo_params, nlo_params, nnlo_params):
     """Creates a submodule with Veff and Veff_params functions (see below).
     """
@@ -94,14 +96,24 @@ def write_veff_function(file, lo_params, nlo_params, nnlo_params):
     nnlo functions and evaluates them, returning the results in a tuple.
     """
     file.write('from .lo import lo\n')
-    file.write('from .nlo import nlo\n')
-    file.write('from .nnlo import nnlo\n')
+
+    if nlo_params is not None:
+        file.write('from .nlo import nlo\n')
+
+    if nnlo_params is not None:
+        file.write('from .nnlo import nnlo\n')
+
     file.write('\n')
     
     # Function name and input
     file.write('def Veff(\n')
     
-    params = np.unique(np.concat((lo_params, nlo_params, nnlo_params)))
+    params = np.unique(np.concatenate((
+        lo_params, 
+        nlo_params if nlo_params is not None else [], 
+        nnlo_params if nnlo_params is not None else [], 
+    )))
+
     for param in params:
         param = convert_to_cython_syntax(param)
         file.write(f'    {param} = 1,\n')
@@ -115,17 +127,23 @@ def write_veff_function(file, lo_params, nlo_params, nnlo_params):
         file.write(f'        {param},\n')
     file.write('    )\n')
     
-    file.write('    val_nlo = nlo(\n')
-    for param in nlo_params:
-        param = convert_to_cython_syntax(param)
-        file.write(f'        {param},\n')
-    file.write('    )\n')
-    
-    file.write('    val_nnlo = nnlo(\n')
-    for param in nnlo_params:
-        param = convert_to_cython_syntax(param)
-        file.write(f'        {param},\n')
-    file.write('    )\n')
+    if nlo_params is not None:
+        file.write('    val_nlo = nlo(\n')
+        for param in nlo_params:
+            param = convert_to_cython_syntax(param)
+            file.write(f'        {param},\n')
+        file.write('    )\n')
+    else:
+        file.write('    val_nlo = 0\n')
+
+    if nnlo_params is not None:
+        file.write('    val_nnlo = nnlo(\n')
+        for param in nnlo_params:
+            param = convert_to_cython_syntax(param)
+            file.write(f'        {param},\n')
+        file.write('    )\n')
+    else:
+        file.write('    val_nnlo = 0\n')
     
     file.write('    return (val_lo, val_nlo, val_nnlo)\n')
     
@@ -139,13 +157,16 @@ def write_veff_params_function(file, lo_params, nlo_params, nnlo_params):
     file.write('def Veff_params(params):\n')
     file.write('    return (\n')
     
-    params = np.unique(np.concat((lo_params, nlo_params, nnlo_params)))
+    params = np.unique(np.concatenate((
+        lo_params, 
+        nlo_params if nlo_params is not None else [], 
+        nnlo_params if nnlo_params is not None else [], 
+    )))
+
     for param in params:
         param = convert_to_cython_syntax(param)
         file.write(f'        params["{param}"],\n')
     file.write('    )\n')
-
-
 
 def generate_lo_submodule(name, filename, lo_file):
     """Creates a cython module with a function that evaluates an expression for
