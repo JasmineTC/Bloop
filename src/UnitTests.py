cc66ae7970b7f9d73230fae2675ad7aa009b9998
Jasmine <jasmine@stp.dias.ie>
2025-08-11 11:32:34 +0100

    bmGen now properly imported for unit tests

--- src/UnitTests.py
+++ src/UnitTests.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     from ThreeHiggs.PythoniseMathematica import PythoniseMathematicaUnitTests
     from ThreeHiggs.ParsedExpression import ParsedExpressionUnitTests
     from ThreeHiggs.TransitionFinder import TransitionFinderUnitTests
-    from ThreeHiggs.BmGenerator import BmGeneratorUnitTests
+    from ThreeHiggs.Z2_ThreeHiggsBmGenerator import BmGeneratorUnitTests
     from ThreeHiggs.DimensionalReduction import DimensionalReductionUnitTest
     from ThreeHiggs.PDGData import PDGUnitTests
     from unittest import main
